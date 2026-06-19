d362e9121df973ce3388db1b9e3226efc985313f
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2026-06-19 10:29:04 +0200

    switch to padel realm

--- Src/WebAPI/Settings.cs
+++ Src/WebAPI/Settings.cs
@@ -5,7 +5,7 @@
         public const string AdminPolicyName       = "AdminUser";
         public const string UserPolicyName        = "User";
         public const string AdminOrUserPolicyName = "AdminOrUser";
-        public const string KeycloakAdminRoleName = "myAdminRole"; // use "myAdminRole" for realm myTest
-        public const string KeycloakUserRoleName  = "myUserRole";  // use "myUserRole" for realm myTest
+        public const string KeycloakAdminRoleName = "adminRole"; 
+        public const string KeycloakUserRoleName  = "userRole";  
     }
 }
