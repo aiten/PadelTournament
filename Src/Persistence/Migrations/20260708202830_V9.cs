06e3068aae5921657563fb12b631f7b0ee021523
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2026-07-08 22:38:16 +0200

    NoTieBreak

---
+++ Src/Persistence/Migrations/20260708202830_V9.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class V9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MinDiff",
+                table: "Formats",
+                newName: "MinMargin");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "NoTiebreak",
+                table: "Formats",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NoTiebreak",
+                table: "Formats");
+
+            migrationBuilder.RenameColumn(
+                name: "MinMargin",
+                table: "Formats",
+                newName: "MinDiff");
+        }
+    }
+}
