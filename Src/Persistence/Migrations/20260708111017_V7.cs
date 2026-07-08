7c0c1d07b0757c0a1a1b4e58aeb737f7ca75ffb5
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2026-07-08 13:29:06 +0200

    migration

---
+++ Src/Persistence/Migrations/20260708111017_V7.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class V7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountType",
+                table: "Tournaments",
+                newName: "Format");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Format",
+                table: "Tournaments",
+                newName: "CountType");
+        }
+    }
+}
